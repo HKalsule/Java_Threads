dd3251920d595d1c6a9874f99b37e4fda1eb5e5b
Hardik Kalsule <hardikslaptop0710@gmail.com>
2025-06-28 10:43:35 +0530

    Add files via upload

---
+++ ThreadUsingExtendingThreadClass.java
@@ -0,0 +1,27 @@
+class MyThread1 extends Thread{
+    public void run(){
+        int i = 0;
+        while(i<5){
+            System.out.println("Thread1 is running");
+            i++;
+    }
+}}
+
+class MyThread2 extends Thread{
+    public void run(){
+        int i = 0;
+        while(i<5){
+            System.out.println("Thread2 is running");
+            i++;
+    }
+}}
+
+public class ThreadUsingExtendingThreadClass {
+    public static void main(String[] args) {
+        MyThread1 t1 = new MyThread1();
+        MyThread2 t2 = new MyThread2();
+        t1.start();
+        t2.start();
+    }
+}
+
